bfae6291a4a94443a46c4f00a1018df3dc23b2c5
zhandos <zhandos.90@gmail.com>
2022-09-18 20:59:08 +0500

    yii2 basic app

---
+++ application/app/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysqldb;dbname=kg',
+    'username' => 'kg',
+    'password' => '111111',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
